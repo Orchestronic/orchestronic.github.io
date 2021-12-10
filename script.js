f1f77bc210a974db109b5363e2e22ddda05e609a
Orchestronic <86896086+Orchestronic@users.noreply.github.com>
2021-12-10 09:27:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const container = document.querySelector('.container');
+const openContainer = document.querySelector('.container.show-nav');
 const open = document.getElementById('open');
 const close = document.getElementById('close');
 const motion = document.getElementById('motion');
 
 open.addEventListener('click', () => {
+ if (window.matchMedia("(max-width: 100px)").matches) {
+  container.classList.add('smallVp');
+ }
+
  container.classList.add('show-nav');
  motion.classList.add('movin');
 })
@@ -13,7 +18,6 @@ close.addEventListener('click', () => {
 
 
 });
-
 
 
 
